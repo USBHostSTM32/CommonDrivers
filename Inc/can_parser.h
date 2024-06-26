3304f52d8bddbd5826b022e7e6f5dea84e3dfac2
Quantum095 <105911822+Quantum095@users.noreply.github.com>
2024-06-26 09:51:48 +0200

    added can parser

---
+++ Inc/can_parser.h
@@ -0,0 +1,16 @@
+/*
+ * can_parser.h
+ *
+ *  Created on: Jun 25, 2024
+ *      Author: vital
+ */
+
+#ifndef INC_CAN_PARSER_H_
+#define INC_CAN_PARSER_H_
+
+
+
+
+
+
+#endif /* INC_CAN_PARSER_H_ */
---
+++ Src/can_parser.c
@@ -0,0 +1,8 @@
+/*
+ * can_parser.c
+ *
+ *  Created on: Jun 25, 2024
+ *      Author: vital
+ */
+
+#include "can_parser.h"
