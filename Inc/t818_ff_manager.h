b21adaa83c6ed727c8ea38a57e00899d0e7e9db7
Quantum095 <105911822+Quantum095@users.noreply.github.com>
2024-07-04 17:21:51 +0200

    added force feedback manager

---
+++ Inc/t818_ff_manager.h
@@ -0,0 +1,18 @@
+/*
+ * t818_ff_manager.h
+ *
+ *  Created on: Jul 4, 2024
+ *      Author: vital
+ */
+
+#ifndef INC_T818_FF_MANAGER_H_
+#define INC_T818_FF_MANAGER_H_
+
+
+
+
+typedef struct{
+
+}t818_ff_manager_t;
+
+#endif /* INC_T818_FF_MANAGER_H_ */
---
+++ Src/t818_ff_manager.c
@@ -0,0 +1,8 @@
+/*
+ * t818_ff_manager.c
+ *
+ *  Created on: Jul 4, 2024
+ *      Author: vital
+ */
+
+#include "t818_ff_manager.h"
