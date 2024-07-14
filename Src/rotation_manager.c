5aeb5b841092ca90b494482832874acade071280
Quantum095 <105911822+Quantum095@users.noreply.github.com>
2024-07-14 16:47:23 +0200

    rotation manager updated

--- Src/rotation_manager.c
+++ Src/rotation_manager.c
@@ -21,7 +21,6 @@ Rotation_Manager_StatusTypeDef rotation_manager_init(
 	return status;
 }
 
-
 Rotation_Manager_StatusTypeDef rotation_manager_update(
 		rotation_manager_t *rotation_manager, int16_t auto_steer_feedback,
 		int16_t auto_control_steer) {
@@ -30,14 +29,14 @@ Rotation_Manager_StatusTypeDef rotation_manager_update(
 	double e = 0.0;
 	if ((rotation_manager != NULL)) {
 		status = ROTATION_MANAGER_OK;
-		e = (((double) auto_steer_feedback / 300.0) * 1024.0)
-				- (double) auto_control_steer;
+		e = (double) map_value_float(auto_steer_feedback, 660.0f, 840.0f,
+				-1024.0f, 1024.0f) - (double) auto_control_steer;
 
 		if (pid_calculate_output(rotation_manager->pid, e, &u) == PID_ERROR) {
 			status = ROTATION_MANAGER_ERROR;
 		}
 
-		int16_t input_force = (int16_t) (u*(-1.0));
+		int16_t input_force = (int16_t) (u * (-1.0));
 
 		if ((t818_ff_manager_upload_costant(rotation_manager->urb_sender,
 				input_force) == T818_FF_MANAGER_ERROR)
