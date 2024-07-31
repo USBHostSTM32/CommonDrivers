d5d0c54ab8bebb42e68d4a46ed4f5c28f8c0894e
Quantum095 <105911822+Quantum095@users.noreply.github.com>
2024-07-31 11:08:21 +0200

    Changed pid values, added dumper, mode, force feedback linearity and force intensity configuration on t818

--- Inc/pid_regulator.h
+++ Inc/pid_regulator.h
@@ -37,9 +37,9 @@ typedef uint8_t PID_StatusTypeDef;
 
 
 
-#define PID_KP			((double)-7.2015)
+#define PID_KP			((double)-4.4795)
 #define PID_KI			((double)0.00)
-#define PID_KD			((double)-10.2427)
+#define PID_KD			((double)-10.2476)
 
 /**
  * @struct pid_t
